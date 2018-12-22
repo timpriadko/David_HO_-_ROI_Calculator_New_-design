ef1ff594237759059a323383b48ae75393078b3a
unknown <timpriadko@gmail.com>
2018-12-22 14:07:00 +0200

    add fixed

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
  // Read more
- function readMoreBtn() {
-     var dots = document.getElementById("dots");
-     var moreText = document.getElementById("more");
-     var btnText = document.getElementById("myBtn");
+
+ $(document).ready(function() {
+     $("#toggle").click(function() {
+         var elem = $("#toggle").text();
+         if (elem == "Read More") {
+             //Stuff to do when btn is in the read more state
+             $("#toggle").text("Read Less");
+             $("#text").slideDown();
+         } else {
+             //Stuff to do when btn is in the read less state
+             $("#toggle").text("Read More");
+             $("#text").slideUp();
+         }
+     });
+ });
+
+ //  function readMoreBtn() {
+ //      var dots = document.getElementById("dots");
+ //      var moreText = document.getElementById("more");
+ //      var btnText = document.getElementById("myBtn");
 
 
-     if (dots.style.display === "none") {
-         dots.style.display = "inline";
-         btnText.innerHTML = "Read more";
-         moreText.style.display = "none";
-     } else {
-         dots.style.display = "none";
-         btnText.innerHTML = "Read less";
-         moreText.style.display = "inline";
-     }
- }
+ //      if (dots.style.display === "none") {
+ //          dots.style.display = "inline";
+ //          dots.style.display = "transition: all 3.2s";
+ //          btnText.innerHTML = "Read more";
+ //          moreText.style.display = "none";
+ //      } else {
+ //          dots.style.display = "none";
+ //          btnText.innerHTML = "Read less";
+ //          moreText.style.display = "inline";
+ //      }
+ //  }
 
  function showhidediv() {
 
